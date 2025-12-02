9281e231dd71f50998ea0bf1aacd2acdb86b51d9
jair <jairestebanca@gmail.com>
2025-12-02 13:34:50 -0500

    integracion con backend

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; // 1. Importar HttpClient
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, HttpClientModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
@@ -14,12 +15,37 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   onLogin() {
     if (this.email && this.password) {
-      console.log('Login:', this.email);
-      this.router.navigate(['/dashboard']);
+      
+      // Preparamos el JSON
+      const credenciales = {
+        email: this.email,
+        password: this.password // Este debe coincidir con lo que espera el AuthController en Java
+      };
+
+      // 4. Petición POST al backend
+      this.http.post('http://localhost:8080/api/auth/login', credenciales)
+        .subscribe({
+          next: (response: any) => {
+            console.log('Login exitoso:', response);
+            
+            // 5. GUARDAR SESIÓN (Básico para MVP)
+            // Guardamos el objeto usuario en el navegador para saber que está logueado
+            localStorage.setItem('usuario', JSON.stringify(response));
+            
+            this.router.navigate(['/dashboard']);
+          },
+          error: (error) => {
+            console.error('Error login:', error);
+            alert('Credenciales incorrectas. Intenta de nuevo.');
+          }
+        });
+
+    } else {
+      alert('Por favor ingresa correo y contraseña');
     }
   }
 }
